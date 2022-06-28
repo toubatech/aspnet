9746fd628ff01aab1f8b9ec8afe7092b82fad000
Poya-darbandi75 <44476275+Poya-darbandi75@users.noreply.github.com>
2022-06-28 12:30:21 +0430

    Update Database && Update Utilities

---
+++ SinaMN75Api/Migrations/20220628075616_2022-06-28.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SinaMN75Api.Migrations
+{
+    public partial class _20220628 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ReadMessage",
+                table: "Chats",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReadMessage",
+                table: "Chats");
+        }
+    }
+}
