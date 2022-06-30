d454fca9b2457e13700053a25576c7ac2b022b16
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2022-06-30 20:40:39 +0430

    :)

---
+++ SinaMN75Api/Migrations/20220630111404_m13.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SinaMN75Api.Migrations
+{
+    public partial class m13 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FolderName",
+                table: "Bookmarks",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FolderName",
+                table: "Bookmarks");
+        }
+    }
+}
