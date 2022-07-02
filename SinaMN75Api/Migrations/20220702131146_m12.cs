793d1c3905cec56851a538771d40b189341da793
Poya-darbandi75 <44476275+Poya-darbandi75@users.noreply.github.com>
2022-07-02 17:42:27 +0430

    Update-database

---
+++ SinaMN75Api/Migrations/20220702131146_m12.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SinaMN75Api.Migrations
+{
+    public partial class m12 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TitleTr2",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TitleTr2",
+                table: "Categories");
+        }
+    }
+}
