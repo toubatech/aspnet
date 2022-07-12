d6c283af490ecb2f68ed021e1c4eecf427f45e71
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2022-07-12 17:29:39 +0430

    add m17

---
+++ SinaMN75Api/Migrations/20220712125647_m17.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SinaMN75Api.Migrations
+{
+    public partial class m17 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Instagram",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LinkedIn",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Telegram",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "WhatsApp",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Instagram",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LinkedIn",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Telegram",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "WhatsApp",
+                table: "AspNetUsers");
+        }
+    }
+}
