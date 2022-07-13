567a995595b584821e87f44eb55582e95f8a1172
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2022-07-13 13:51:17 +0430

    update

---
+++ SinaMN75Api/Migrations/20220713085845_m18.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SinaMN75Api.Migrations
+{
+    public partial class m18 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Contents_AspNetUsers_UserId",
+                table: "Contents");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Contents_UserId",
+                table: "Contents");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Contents");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Amount",
+                table: "Transactions",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Price",
+                table: "Products",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Price",
+                table: "Order",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Point",
+                table: "AspNetUsers",
+                type: "float",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Point",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                table: "Transactions",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Products",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Order",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Contents",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contents_UserId",
+                table: "Contents",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Contents_AspNetUsers_UserId",
+                table: "Contents",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
