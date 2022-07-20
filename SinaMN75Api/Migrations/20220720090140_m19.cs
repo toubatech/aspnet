1bd6babd1bd9a631713850021465b02759622a56
Poya-darbandi75 <44476275+Poya-darbandi75@users.noreply.github.com>
2022-07-20 13:33:09 +0430

    Update DataBase & Utilities

---
+++ SinaMN75Api/Migrations/20220720090140_m19.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SinaMN75Api.Migrations
+{
+    public partial class m19 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "State",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "State",
+                table: "AspNetUsers");
+        }
+    }
+}
