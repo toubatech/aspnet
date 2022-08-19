220088d7ab27b0a6800ac81564cc6f44f8b45399
Neda Mohammadi <neda.mohammadi88@gmail.com>
2022-08-19 20:14:51 +0430

    config for delete

--- SinaMN75Api/Core/AppDbContext.cs
+++ SinaMN75Api/Core/AppDbContext.cs
@@ -2,37 +2,42 @@
 
 namespace SinaMN75Api.Core;
 
-public class AppDbContext : IdentityDbContext<UserEntity> {
-	public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+public class AppDbContext : IdentityDbContext<UserEntity>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-	public DbSet<ProductEntity> Products { get; set; }
-	public DbSet<TransactionEntity> Transactions { get; set; }
-	public DbSet<ReportEntity> Reports { get; set; }
-	public DbSet<NotificationEntity> Notifications { get; set; }
-	public DbSet<FormEntity> Forms { get; set; }
-	public DbSet<FollowEntity> Follows { get; set; }
-	public DbSet<BookmarkEntity> Bookmarks { get; set; }
-	public DbSet<MediaEntity> Media { get; set; }
-	public DbSet<TeamEntity> Teams { get; set; }
-	public DbSet<VoteEntity> Votes { get; set; }
-	public DbSet<VoteFieldEntity> VoteFields { get; set; }
-	public DbSet<ContentEntity> Contents { get; set; }
-	public DbSet<OtpEntity> Otps { get; set; }
-	public DbSet<LocationEntity> Locations { get; set; }
-	public DbSet<CategoryEntity> Categories { get; set; }
-	public DbSet<FormFieldEntity> FormFields { get; set; }
-	public DbSet<ChatEntity> Chats { get; set; }
-	public DbSet<CommentEntity> Comments { get; set; }
-	public DbSet<BlockEntity> Blocks { get; set; }
-	public DbSet<OrderEntity> Orders { get; set; }
-	public DbSet<TopProductEntity> TopProducts { get; set; }
-	public DbSet<DiscountEntity> Discounts { get; set; }
+    public DbSet<ProductEntity> Products { get; set; }
+    public DbSet<TransactionEntity> Transactions { get; set; }
+    public DbSet<ReportEntity> Reports { get; set; }
+    public DbSet<NotificationEntity> Notifications { get; set; }
+    public DbSet<FormEntity> Forms { get; set; }
+    public DbSet<FollowEntity> Follows { get; set; }
+    public DbSet<BookmarkEntity> Bookmarks { get; set; }
+    public DbSet<MediaEntity> Media { get; set; }
+    public DbSet<TeamEntity> Teams { get; set; }
+    public DbSet<VoteEntity> Votes { get; set; }
+    public DbSet<VoteFieldEntity> VoteFields { get; set; }
+    public DbSet<ContentEntity> Contents { get; set; }
+    public DbSet<OtpEntity> Otps { get; set; }
+    public DbSet<LocationEntity> Locations { get; set; }
+    public DbSet<CategoryEntity> Categories { get; set; }
+    public DbSet<FormFieldEntity> FormFields { get; set; }
+    public DbSet<ChatEntity> Chats { get; set; }
+    public DbSet<CommentEntity> Comments { get; set; }
+    public DbSet<BlockEntity> Blocks { get; set; }
+    public DbSet<OrderEntity> Orders { get; set; }
+    public DbSet<TopProductEntity> TopProducts { get; set; }
+    public DbSet<DiscountEntity> Discounts { get; set; }
 
-	protected override void OnModelCreating(ModelBuilder modelBuilder)
-	{
-		base.OnModelCreating(modelBuilder);
 
-		modelBuilder.Entity<CommentEntity>().HasMany(e => e.Children).WithOne().OnDelete(DeleteBehavior.Cascade);
-	}
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+        {
+            relationship.DeleteBehavior = DeleteBehavior.ClientCascade;
+        } 
+    }
 }
 
