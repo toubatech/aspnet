7ae180d364b135b0a97071dc128807fd6d00f318
Neda Mohammadi <neda.mohammadi88@gmail.com>
2022-08-22 22:22:37 +0430

    migration add access feild for user

---
+++ SinaMN75Api/Migrations/20220822175100_mig_add_access_field.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SinaMN75Api.Migrations
+{
+    public partial class mig_add_access_field : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Access",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Access",
+                table: "AspNetUsers");
+        }
+    }
+}
