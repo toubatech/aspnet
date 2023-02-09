b87214fba2c954b34ea9abcc33a3be533b629e2e
MazDot <hamedmaz400@gmail.com>
2023-02-09 20:59:53 +0330

    chatMessage entity updated

--- SinaMN75Api/Models/ChatMessage.cs
+++ SinaMN75Api/Models/ChatMessage.cs
@@ -6,11 +6,13 @@ namespace SinaMN75Api.Models
 	{
 		public string FromUserId { get; set; } = null!;
 		public string ToUserId { get; set; } = null!;
+        public Guid ToGroupId { get; set; }
 
-		[StringLength(2000)]
+        [StringLength(2000)]
 		public string MessageText { get; set; } = null!;
 
 		public bool ReadMessage { get; set; }
         public List<Emoji> Emojies { get; set; } = new List<Emoji>();
+        public Guid RepliedTo { get; set; }
     }
 }
