ab7ad7af734d12c7249eea6b6e00346289edfd34
Mfarsi138 <mohammad.farsi138@gmail.com>
2023-02-17 15:35:25 +0330

    order topkeyword

--- SinaMN75Api/Controllers/PaymentController.cs
+++ SinaMN75Api/Controllers/PaymentController.cs
@@ -1,4 +1,6 @@
-﻿namespace SinaMN75Api.Controllers
+﻿
+
+namespace SinaMN75Api.Controllers
 {
     [Route("[controller]")]
     public class PaymentController : Controller
@@ -20,10 +22,10 @@
         [ApiExplorerSettings(IgnoreApi = true)]
         [HttpGet("WalletCallBack/{userId}/{amount:int}")]
         [HttpPost("WalletCallBack/{userId}/{amount:int}")]
-        public async Task<IActionResult> WalletCallBack(string userId ,int amount, string authority, string status)
+        public async Task<IActionResult> WalletCallBack(string userId, int amount, string authority, string status)
         {
             GenericResponse i = await _paymentRepository.WalletCallBack(amount, authority, status, userId, ZarinPalMerchantId);
-            if(i.Status == UtilitiesStatusCodes.Success)
+            if (i.Status == UtilitiesStatusCodes.Success)
             {
                 return RedirectToAction(nameof(Verify));
             }
@@ -64,5 +66,68 @@
             }
             return RedirectToAction(nameof(Fail));
         }
+
+
+
+      //  [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [HttpGet("PayOrderStripe/{orderId}")]
+        public async Task<GenericResponse<string?>> PayOrderStripe(Guid orderId)
+        {
+            string param = "";
+           // GenericResponse<string?> i = await _paymentRepository.PayOrderStripe(orderId, param);
+            return null;
+        }
+
+        [ApiExplorerSettings(IgnoreApi = true)]
+        [HttpGet("CallBackOrderStripe/{orderId:guid}")]
+        [HttpPost("CallBackOrderStripe/{orderId:guid}")]
+        public async Task<IActionResult> CallBackOrderStripe(Guid productId, string authority, string status)
+        {
+            GenericResponse i = await _paymentRepository.CallBack(productId, authority, status, ZarinPalMerchantId);
+            if (i.Status == UtilitiesStatusCodes.Success)
+            {
+                return RedirectToAction(nameof(Verify));
+            }
+            return RedirectToAction(nameof(Fail));
+        }
+
+        const string secret = "whsec_...";
+
+        //[HttpPost]
+        //public async Task<IActionResult> Index()
+        //{
+        //    var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+
+        //    try
+        //    {
+        //        var stripeEvent = EventUtility.ConstructEvent(
+        //          json,
+        //          Request.Headers["Stripe-Signature"],
+        //          secret
+        //        );
+
+        //        // Handle the checkout.session.completed event
+        //        if (stripeEvent.Type == Events.CheckoutSessionCompleted)
+        //        {
+        //            var session = stripeEvent.Data.Object;//as Checkout.Session;
+        //            var options = new SessionGetOptions();
+        //            options.AddExpand("line_items");
+
+        //            var service = new SessionService();
+        //            // Retrieve the session. If you require line items in the response, you may include them by expanding line_items.
+        //            Session sessionWithLineItems = service.Get("session.Id", options);
+        //            StripeList<LineItem> lineItems = sessionWithLineItems.LineItems;
+
+        //            // Fulfill the purchase...
+        //            //this.FulfillOrder(lineItems);
+        //        }
+
+        //        return Ok();
+        //    }
+        //    catch (StripeException e)
+        //    {
+        //        return BadRequest();
+        //    }
+        //}
     }
 }
