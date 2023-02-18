a8387ffd51de43179570dcc758c6ac013bd0674f
Mohamadhosein <mhroohiamini@gmail.com>
2023-02-18 12:06:53 +0330

    setting up signalR

--- SinaMN75Api/Program.cs
+++ SinaMN75Api/Program.cs
@@ -1,13 +1,16 @@
 using SinaMN75Api.Core;
+using Utilities_aspnet.Hubs;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.SetupUtilities<AppDbContext>(builder.Configuration.GetConnectionString("ServerSQLServer"));
+builder.Services.AddSignalR();
 
 WebApplication app = builder.Build();
 
 app.UseUtilitiesServices();
 
 app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+app.MapHub<ChatHub>("/hubs/ChatHub");
 
 app.Run();
