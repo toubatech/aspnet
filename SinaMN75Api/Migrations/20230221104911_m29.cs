03e087dd96099d086f366ffe17c8656567b71f2f
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2023-02-21 14:19:31 +0330

    :)

---
+++ SinaMN75Api/Migrations/20230221104911_m29.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SinaMN75Api.Migrations
+{
+    public partial class m29 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
