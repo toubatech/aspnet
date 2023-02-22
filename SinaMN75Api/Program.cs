c5253b85c896c961f950de929097e5fe8c4fe45f
Mohamadhosein <mhroohiamini@gmail.com>
2023-02-22 20:41:47 +0330

    remove signal R config and move to utilities

--- SinaMN75Api/Program.cs
+++ SinaMN75Api/Program.cs
@@ -4,13 +4,11 @@ using Utilities_aspnet.Hubs;
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.SetupUtilities<AppDbContext>(builder.Configuration.GetConnectionString("ServerSQLServer"));
-builder.Services.AddSignalR();
 
 WebApplication app = builder.Build();
 
 app.UseUtilitiesServices();
 
 app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
-app.MapHub<ChatHub>("/hubs/ChatHub");
 
 app.Run();
