864ad9bb7224ac8a6379a2b87acda0d4b10f807c
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2023-02-25 10:37:13 +0330

    :(

---
+++ SinaMN75Api/Migrations/20230225070638_m36.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SinaMN75Api.Migrations
+{
+    public partial class m36 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SeenUsers",
+                table: "Products",
+                type: "nvarchar(max)",
+                maxLength: 100000,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SeenUsers",
+                table: "Products");
+        }
+    }
+}
